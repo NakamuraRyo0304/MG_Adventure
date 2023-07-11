b11479d3425228064419560b3f780a999d6abdd5
NakamuraRyo0304 <shino.nakiri@gmail.com>
2023-07-12 00:36:24 +0900

    無駄なコードを削除、アプリ名を変更

--- Main.cpp
+++ Main.cpp
@@ -219,30 +219,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
 
- /*   case WM_SIZE:
-        if (wParam == SIZE_MINIMIZED)
-        {
-            if (!s_minimized)
-            {
-                s_minimized = true;
-                if (!s_in_suspend && game)
-                    game->OnSuspending();
-                s_in_suspend = true;
-            }
-        }
-        else if (s_minimized)
-        {
-            s_minimized = false;
-            if (s_in_suspend && game)
-                game->OnResuming();
-            s_in_suspend = false;
-        }
-        else if (!s_in_sizemove && game)
-        {
-            game->OnWindowSizeChanged(LOWORD(lParam), HIWORD(lParam));
-        }
-        break;*/
-
     case WM_ENTERSIZEMOVE:
         s_in_sizemove = true;
         break;
