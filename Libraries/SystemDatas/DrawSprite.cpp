04bd6d05b5831405158fa4a3988a036323784ea6
NakamuraRyo0304 <shino.nakiri@gmail.com>
2023-08-18 23:34:57 +0900

    少しリファクタリング

--- Libraries/SystemDatas/DrawSprite.cpp
+++ Libraries/SystemDatas/DrawSprite.cpp
@@ -76,7 +76,7 @@ void DrawSprite::AddTextureData(const wchar_t* key, const wchar_t* path ,ID3D11D
 /// <param name="origin">’†SˆÊ’u</param>
 /// <param name="rect">Ø‚èæ‚èˆÊ’u</param>
 /// <returns>‚È‚µ</returns>
-void DrawSprite::DrawTexture(const wchar_t* key, SimpleMath::Vector2 pos, 
+void DrawSprite::DrawTexture(const wchar_t* key, SimpleMath::Vector2 pos,
 	SimpleMath::Vector4 color, float rate, SimpleMath::Vector2 origin, RECT_U rect)
 {
 	m_spriteBatch->Begin();
