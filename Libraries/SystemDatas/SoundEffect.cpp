5e5d6dcd5f7c25d855d9d8606250e40c25228516
NakamuraRyo0304 <shino.nakiri@gmail.com>
2023-09-12 14:42:43 +0900

    関数定義の作成、コメント追記

--- Libraries/SystemDatas/SoundEffect.cpp
+++ Libraries/SystemDatas/SoundEffect.cpp
@@ -7,7 +7,41 @@
 
 #include "pch.h"
 
-#include "X3DAudio.h"
+#include "xaudio2.h"
 
 #include "SoundEffect.h"
 
+ /// <summary>
+ /// コンストラクタ
+ /// </summary>
+ /// <param name="引数無し"></param>
+ /// <returns>なし</returns>
+SoundEffect::SoundEffect()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+/// <param name="引数無し"></param>
+/// <returns>なし</returns>
+SoundEffect::~SoundEffect()
+{
+}
+
+/// <summary>
+/// リソース保存関数
+/// </summary>
+/// <param name="key">登録キー</param>
+/// <param name="path">リソースのパス</param>
+/// <returns>なし</returns>
+void SoundEffect::AddSoundEffects(const wchar_t* key, const wchar_t* path)
+{
+	// キーパスの保存
+	m_sounds.emplace(key, path);
+
+	// 対象パスを確保
+	std::map<const wchar_t*, const wchar_t*>::const_iterator it = m_sounds.find(key);
+
+
+}
--- Libraries/SystemDatas/SoundEffect.h
+++ Libraries/SystemDatas/SoundEffect.h
@@ -15,12 +15,16 @@ class SoundEffect
 {
 private:
 	// 音源の格納配列
-	std::map<const wchar_t*, const wchar_t*> m_textures;
+	std::map<const wchar_t*, const wchar_t*> m_sounds;
 
 	// サウンドへのポインタ
 
 public:
+	SoundEffect();
+	~SoundEffect();
 
+	// リソース追加関数
+	void AddSoundEffects(const wchar_t* key, const wchar_t* path);
 };
 
 #endif // SOUNDEFFECT
